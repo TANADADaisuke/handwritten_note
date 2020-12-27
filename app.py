0ddc389308d6a86ef9dedb365914c23cb7ac9fcb
Tanada Daisuke <nvkx873110@gmail.com>
2020-12-27 19:45:40 +0900

    application is required

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+
+    # App Config
+    app = Flask(__name__)
+
+    # ---------------------------------------- #
+    # Endpoints
+    # ---------------------------------------- #
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
